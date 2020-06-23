d5947a51c26897b967bc4cab0994016aa6db640a
a8s <abhishek7soni@gmail.com>
2020-06-24 03:33:47 +0530

    .

--- client/src/Components/variables.js
+++ client/src/Components/variables.js
@@ -1,5 +1,5 @@
-export const serverUrl = 'http://localhost:8080';
+export const serverUrl = 'http://localhost:5000';
 export const clientUrl = 'http://localhost:3000';
 // export const serverUrl = '${serverUrl}';
 
-export const admins = [ 'sourabh.tripathi48@gmail.com' ];
+export const admins = ['sourabh.tripathi48@gmail.com'];
