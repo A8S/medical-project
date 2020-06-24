6728a09fc9c6872b574f200f3b043279abd5fd92
a8s <abhishek7soni@gmail.com>
2020-06-24 17:44:29 +0530

    .

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,4 +1,5 @@
-const express = require("express");
+const express = require('express');
+var cors = require('cors');
 const {
   getPosts,
   createPost,
@@ -11,44 +12,49 @@ const {
   singlePost,
   like,
   unlike,
-  tags
-} = require("../controllers/post");
-const { requireSignin } = require("../controllers/auth");
-const { userById } = require("../controllers/user");
-const { createPostValidator } = require("../validator");
+  tags,
+} = require('../controllers/post');
+const { requireSignin } = require('../controllers/auth');
+const { userById } = require('../controllers/user');
+const { createPostValidator } = require('../validator');
 // const postController = require("../controllers/post");
 // router.get("/", postController.getPosts);
 // router.post("/post", postController.createPost);
 const router = express.Router();
 
-router.get("/posts", getPosts);
+var corsOptions = {
+  origin: 'http://example.com',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+router.get('/posts', getPosts);
 
 // like unlike
-router.put("/post/like", like);
-router.put("/post/unlike",  unlike);
+router.put('/post/like', like);
+router.put('/post/unlike', unlike);
 //tags
-router.post("/post/tags",tags);
+router.post('/post/tags', tags);
 
 router.post(
   // use requireSignin as middleware
-  "/post/new/:userId", // in order to post something we need userId
-  
+  '/post/new/:userId', // in order to post something we need userId
+
   createPost,
   createPostValidator // this validator should be at last bz then it shows error in the starting only of title & body
 );
-router.get("/posts/by/:userId", requireSignin, postsByUser);
+router.get('/posts/by/:userId', requireSignin, postsByUser);
 
- // no mandatory to add requireSignin as userId is there in params
-router.get("/post/:postId", singlePost);
-router.put("/post/:postId", requireSignin, isPoster, updatePost);
-router.delete("/post/:postId", requireSignin, isPoster, deletePost);
+// no mandatory to add requireSignin as userId is there in params
+router.get('/post/:postId', cors(corsOptions), singlePost);
+router.put('/post/:postId', requireSignin, isPoster, updatePost);
+router.delete('/post/:postId', requireSignin, isPoster, deletePost);
 
 // photo
-router.get("/post/photo/:postId", photo);
+router.get('/post/photo/:postId', photo);
 
 // any route containing :userId, our app will first execute userById()
-router.param("userId", userById);
+router.param('userId', userById);
 // any route containing :postId, our app will first execute postById()
-router.param("postId", postById);
+router.param('postId', postById);
 
 module.exports = router;
