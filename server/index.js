54462a439f7146fbda9fd08b70fca0ecf7ab2269
a8s <abhishek7soni@gmail.com>
2020-06-24 13:47:53 +0530

    .

--- server/index.js
+++ server/index.js
@@ -59,7 +59,6 @@ if (process.env.NODE_ENV === 'production') {
   app.get('/*', (req, res) => {
     res.sendfile(path.join((__dirname = 'client/build/index.html')));
   });
-  app.use(express.static('client/build'));
   app.use(express.static(path.join(__dirname, 'client/build')));
 }
 app.use(cookieParser()); // cookieParser(secret, options)
