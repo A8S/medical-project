cd1f393c2f44cbd9069818b54fb0b458a8e6853a
a8s <abhishek7soni@gmail.com>
2020-06-24 17:30:33 +0530

    cors header

--- server/index.js
+++ server/index.js
@@ -61,6 +61,16 @@ app.use(express.json());
 app.use(cookieParser()); // cookieParser(secret, options)
 app.use(expressValidator());
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+
+  next();
+});
 // app.get('/', postRoutes.getPosts);
 app.use('/api', postRoutes);
 app.use('/api', authRoutes);
