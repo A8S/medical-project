61469ffd94216a950853735013242c44946308d6
Abhishek Soni <Abhishek7soni@gmail.com>
2020-06-27 18:29:21 +0530

    delete1

--- server/config.js
+++ server/config.js
@@ -2,5 +2,5 @@ const config = {
   mongoUrl:
     'mongodb+srv://medical:medical123@cluster0-tcfrr.mongodb.net/medical_counselling?retryWrites=true&w=majority',
 };
-
+//// mongodb+srv://medical:medical123@cluster0-tcfrr.mongodb.net/medical?retryWrites=true&w=majority
 module.exports = config;
