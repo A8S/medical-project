9a24739919773f9a0379e805557c4c6d081fbd59
Abhishek Soni <33037679+A8S@users.noreply.github.com>
2020-07-11 22:04:18 +0530

    Update variables.js

--- client/src/variables.js
+++ client/src/variables.js
@@ -1,6 +1,7 @@
 export const serverUrl = 'https://infinite-harbor-95705.herokuapp.com';
 // export const serverUrl = 'http://localhost:5000';
-export const clientUrl = 'http://localhost:3000';
+//export const clientUrl = 'http://localhost:3000';
 // export const serverUrl = '${serverUrl}';
+export const clientUrl = 'https://gifted-gates-e3aa20.netlify.app';
 
 export const admins = ['Abhishek7soni@gmail.com'];
